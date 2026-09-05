4e104ca149d75f8a95b571a28e737ad7fd4815c7
Annika Holmqvist <annika.holmqvist94@gmail.com>
2026-09-05 17:28:39 +0200

    Build exercise detail view with not-found state

--- src/views/ExerciseDetailView.vue
+++ src/views/ExerciseDetailView.vue
@@ -1,9 +1,32 @@
 <script setup>
-defineProps(['id'])
+import { computed } from 'vue'
+import { useExercises } from '../composables/useExercises.js'
+
+const props = defineProps(['id'])
+const { getExerciseById, toggleDone, removeExercise } = useExercises()
+
+const exercise = computed(() => getExerciseById(props.id))
 </script>
 
 <template>
-  <h1>Övning #{{ id }}</h1>
+  <section v-if="exercise">
+    <h1>{{ exercise.name }}</h1>
+    <p>{{ exercise.sets }} sets x {{ exercise.reps }} reps</p>
+    <p v-if="exercise.note">Kommentar: {{ exercise.note }}</p>
+    <p>Status: {{ exercise.done ? 'Klar' : 'Ej klar' }}</p>
+
+    <button type="button" @click="toggleDone(exercise.id)">
+      {{ exercise.done ? 'Ångra' : 'Markera klar' }}
+    </button>
+    <button type="button" @click="removeExercise(exercise.id)">Ta bort</button>
+  </section>
+
+  <section v-else>
+    <h1>Övningen hittades inte</h1>
+    <p>Den kan ha tagits bort.</p>
+  </section>
+
+  <RouterLink :to="{ name: 'program' }">Tillbaka till programmet</RouterLink>
 </template>
 
 <style scoped>
